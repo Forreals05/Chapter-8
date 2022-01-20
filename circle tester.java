0a71c4ed8ede88d292cd5e975ce1f38e69fb2a48
Forreals05 <90797148+Forreals05@users.noreply.github.com>
2022-01-20 16:35:45 -0500

    Update circle tester.java

--- circle tester.java
+++ circle tester.java
@@ -12,28 +12,41 @@ public class CircleTester
         //that is the variable name
         //new is a keyword that INSTANTIATES the object
         //this createx the new CIRCLE object on your RAM
-        //it allocates space for your instance to live
+        //it allocates space for your instance to live on
         //Circle() which calls the CONSTRUCTOR in the CIRCLE class
         //in this case we call the default constructor
-        
+        //Circle() - this is our constructor, used to INTIALIZE our
+        //instance variable
         
         System.out.println(pizza.getRadius());
-        pizza.setRadius(8);
+        pizza.setRadius(6);
         System.out.println(pizza.getRadius());
         //System.out.println(pizza.radius); this will not work; private
         
         
-        Circle token = new Circle(3);
+        Circle token = new Circle(.5);
         Circle pie = new Circle(10);
         Circle clock = new Circle(99);
         
         System.out.println("Token" + token.getRadius());
         System.out.println("Pie" + pie.getRadius());
         System.out.println("Clock" + clock.getRadius());
+        System.out.println();
+        System.out.println("Token A=" + token.getArea());
+        System.out.println("Pizza A=" + pizza.getArea());
         
-        token.setRadius(2);
+        
+        
+        pizza.displayAreaFormula();
+        Circle.displayAreaFormula(); // because this is a static method it can be called from
+        // the class itself instead of from a specific insatnce of the class
+        
+        /* token.setRadius(2);
         double x = 7.9;
         token.setRadius(x);
         token.setRadius(Math.random());
+        */
+       
+       
     }
 }
