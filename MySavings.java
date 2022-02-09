5faf18603c2d1efaf263af23dad2f9baaf25cc65
Forreals05 <90797148+Forreals05@users.noreply.github.com>
2022-02-09 15:02:57 -0500

    Update MySavings.java

--- MySavings.java
+++ MySavings.java
@@ -8,19 +8,27 @@ public class MySavings
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        
+
         int pick = -1;
-    
+
         PiggyBank glenda = new PiggyBank();
-    
-        System.out.println(glenda.toString());
-        
-        System.out.print("Your choice: ");
-        pick = input.nextInt();
-    
-        if (pick == 1)
+
+        System.out.println("1. Show total in bank.\n2. Add a penny.\n3. Add a nickel.\n4. Add a dime.\n5.Add a quarter.\n6. Take money out of bank.\nEnter 0 to quit.");
+
+        do
         {
-            System.out.println("You have "  + " in your bank");
+            System.out.print("Your choice: ");
+            pick = input.nextInt();
+
+            if (pick == 1)
+            {
+                System.out.println(glenda.smashBank());
+            }
+            else if(pick == 2)
+            {
+                glenda.addPenny();
+            }
         }
+        while(pick != 0);
     }
 }
