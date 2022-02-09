a69df9880214a6b7e6634c42c336298147e08b79
Forreals05 <90797148+Forreals05@users.noreply.github.com>
2022-02-09 15:04:15 -0500

    Update PiggyBank.java

--- PiggyBank.java
+++ PiggyBank.java
@@ -1,43 +1,39 @@
 //Alphonso Martin
 //Programming II
 
+import java.text.NumberFormat;
 public class PiggyBank
 {
-     private double total;
-     
-     public PiggyBank()
-     {
-         total = 0;
-     }
-     public PiggyBank(double t)
-     {
-         total = t;
-     }
-     
-     public String toString()
-     {
-        return "1. Show total in bank.\n2. Add a penny.\n3. Add a nickel.\n4. Add a dime.\n5.Add a quarter.\n6. Take money out of bank.\nEnter 0 to quit.";
-     }
-     
-     void addPenny()
-     {
-         total = 0.01 + total;
-         
-     }
-     void addNickel()
-     {
-         total = 0.05 + total;
-     }
-     void addDime()
-     {
-         total = 0.10 + total;
-     }
-     void addQuarter()
-     {
-         total = 0.25 + total;
-     }
-     void smashBank()
-     {
-         total = total - total;
-     }
+    NumberFormat money = NumberFormat.getCurrencyInstance(); 
+    private double total;
+
+    public PiggyBank()
+    {
+        total = 0.0;
+    }
+
+    public void addPenny()
+    {
+        total += 0.01;
+    }
+
+    public void addNickel()
+    {
+        total = 0.05 + total;
+    }
+
+    void addDime()
+    {
+        total = 0.10 + total;
+    }
+
+    void addQuarter()
+    {
+        total = 0.25 + total;
+    }
+
+    public String smashBank()
+    {
+        return "You have " + money.format(total); 
+    }
 }
