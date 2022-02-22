6328bc4629349cb9d1a9d645f96dd9ea75234bf3
Forreals05 <90797148+Forreals05@users.noreply.github.com>
2022-02-22 15:10:10 -0500

    Create Lunchtester.java

---
+++ Lunchtester.java
@@ -0,0 +1,16 @@
+//Alphonso Mart
+//Programming II
+
+public class LunchTester
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Item          Price       Fat(g)  Carbs(g) Fiber(g)");
+        System.out.println("hamburger     $1.85         9      33        1 ");
+        System.out.println("salad         $2.00         1      11        5 ");
+        System.out.println("french fries  $1.30         11     36        4 ");
+        System.out.println("soda          $0.95         0      38        0");
+        
+        
+    }
+}
