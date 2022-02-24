a3ad03eff027c82cbe784e3c7a1ed93796c8a2b8
Forreals05 <90797148+Forreals05@users.noreply.github.com>
2022-02-24 15:11:57 -0500

    Create compareNumbertester.java

---
+++ compareNumbertester.java
@@ -0,0 +1,19 @@
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class NumberCompareRunner
+{
+    public static void main( String args[] )
+    {
+       NumberCompare test = new NumberCompare(5,6);
+      System.out.println(test);
+      System.out.println("largest == "+test.getLargest());
+       System.out.println("smallest == "+test.getSmallest()+"\n\n");
+
+        //add more test cases
+
+
+    }
+}
